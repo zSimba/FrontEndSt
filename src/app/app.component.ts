1ae5ae171a1d7cfb129c73f309096f660f043e1b
Dario Alvarez <darcalzadilla@gmail.com>
2024-02-21 16:31:09 +0100

    Antes de aplicar rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./shared/header/header.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {FooterComponent} from "./shared/footer/footer.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {CreateComponent} from "./pages/create/create.component";
+import {NavComponent} from "./shared/nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, DashboardComponent, FooterComponent, LoginComponent, CreateComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./auth/login/login.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'dashboard',
+    component: DashboardComponent
+  }
+];
